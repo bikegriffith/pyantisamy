ab3e0ef9b3526aabc522c4dbc8bb1c65523b436c
michaeldavidgriffith <michaeldavidgriffith@ae0399de-f498-11dd-ac65-f74f249cfaac>
2009-02-06 21:58:01 +0000

    Initial commit to set up package and distutils

---
+++ trunk/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyantisamy',
+    version='0.1',
+    packages=['owasp',],
+)
+
+#
+# execute the following to create the egg
+# $ python setup.py bdist_egg
+#
