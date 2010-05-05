1b5a564d0857dd308adf2c73bfc6425573e54571
michaeldavidgriffith <michaeldavidgriffith@ae0399de-f498-11dd-ac65-f74f249cfaac>
2010-05-05 01:18:43 +0000

    initial swing at parsing attributes

--- owasp/antisamy/tests/test_policy.py
+++ owasp/antisamy/tests/test_policy.py
@@ -24,11 +24,11 @@ class TestPolicyParserForCoreAntisamyXml(object):
     def test_objectifies_policy_file(self):
         assert self.parser.xml is not None
 
-    def test_parses_common_regular_expressions(self):
+    def test_parses_all_common_regular_expressions(self):
         assert self.policy.regexps
         NT.assert_equals(len(self.policy.regexps), 34)
 
-    def test_parses_directives(self):
+    def test_parses_all_directives(self):
         assert self.policy.directives
         NT.assert_equals(len(self.policy.directives), 8)
 
@@ -42,4 +42,27 @@ class TestPolicyParserForCoreAntisamyXml(object):
         NT.assert_equals(len(self.policy.global_attributes), 5)
         NT.assert_equals(self.policy.global_attributes,
                 ["id", "style", "title", "class", "lang"])
+
+    def test_parses_all_attributes(self):
+        assert self.policy.attributes
+        NT.assert_equals(len(self.policy.attributes), 42)
+
+    def test_parsed_attributes_should_have_regular_expression_validators(self):
+        NT.assert_equals(self.policy.attributes["class"].valid_regexps,
+                ["htmlClass"])
+
+    def test_parsed_attributes_should_have_a_list_of_valid_literals(self):
+        NT.assert_equals(self.policy.attributes["media"].valid_literals,
+                ["screen", "tty", "tv", "projection", "handheld", "print",
+                        "braille", "aural", "all"])
+
+    def test_parsed_attributes_should_have_both_regexp_and_literal_validators(self):
+        attribute = self.policy.attributes["href"]
+        NT.assert_equals(attribute.valid_regexps,
+                ["onsiteURL", "offsiteURL"])
+        NT.assert_equals(attribute.valid_literals,
+                ["javascript:history.go(0)",
+                 "javascript:history.go(-1)",
+                 "javascript:void(0)",
+                 "javascript:location.reload()"])
 
